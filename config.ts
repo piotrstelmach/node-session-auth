82f56fb18295c4f8193d600619b6682a9eace1b8
piotrstelmach <stelmachpiotr46@gmail.com>
2023-11-04 19:12:02 +0100

    Redis connection config

---
+++ config.ts
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+import RedisStore from "connect-redis";
+import {createClient} from "redis";
+
+dotenv.config();
+
+type globalConfig = {
+    REDIS_PORT: string | undefined,
+    REDIS_HOST: string | undefined,
+    MYSQL_ROOT_PASSWORD: string | undefined,
+    MYSQL_DATABASE: string | undefined,
+    MYSQL_USER: string | undefined,
+    MYSQL_PASSWORD: string | undefined,
+}
+
+export const globalConfig: globalConfig = {
+    REDIS_PORT: process.env.REDIS_PORT,
+    REDIS_HOST: process.env.REDIS_HOST,
+    MYSQL_ROOT_PASSWORD: process.env.MYSQL_ROOT_PASSWORD,
+    MYSQL_DATABASE: process.env.MYSQL_DATABASE,
+    MYSQL_USER: process.env.MYSQL_USER,
+    MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+}
+
+export const redisClient = createClient({
+    url: `redis://${globalConfig.REDIS_HOST}:${globalConfig.REDIS_PORT}`
+})
--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
-import express, { Express, Request, Response , Application } from 'express';
+import express, {Express, Request, Response, Application} from 'express';
 import dotenv from 'dotenv';
+import {redisClient} from "./config";
+import RedisStore from "connect-redis";
 
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
+redisClient.connect().then(() => console.log('Redis connected!')).catch(console.error)
 app.get('/', (req: Request, res: Response) => {
     res.send('Welcome to Express & TypeScript Server');
 });

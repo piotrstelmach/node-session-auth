77c782d41d6d4fd11a391821d1511f71742a5e6e
piotrstelmach <stelmachpiotr46@gmail.com>
2023-11-06 20:39:05 +0100

    Configuration eslint, prettier and format existing code.

--- config.ts
+++ config.ts
@@ -1,31 +1,31 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client';
-import {createClient} from "redis";
+import { createClient } from 'redis';
 
 dotenv.config();
 
 type globalConfig = {
-    REDIS_PORT: string | undefined,
-    REDIS_HOST: string | undefined,
-    MYSQL_ROOT_PASSWORD: string | undefined,
-    MYSQL_DATABASE: string | undefined,
-    MYSQL_USER: string | undefined,
-    MYSQL_PASSWORD: string | undefined,
-    REDIS_SECRET: string,
-}
+  REDIS_PORT: string | undefined;
+  REDIS_HOST: string | undefined;
+  MYSQL_ROOT_PASSWORD: string | undefined;
+  MYSQL_DATABASE: string | undefined;
+  MYSQL_USER: string | undefined;
+  MYSQL_PASSWORD: string | undefined;
+  REDIS_SECRET: string;
+};
 
 export const globalConfig: globalConfig = {
-    REDIS_PORT: process.env.REDIS_PORT,
-    REDIS_HOST: process.env.REDIS_HOST,
-    MYSQL_ROOT_PASSWORD: process.env.MYSQL_ROOT_PASSWORD,
-    MYSQL_DATABASE: process.env.MYSQL_DATABASE,
-    MYSQL_USER: process.env.MYSQL_USER,
-    MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
-    REDIS_SECRET: process.env.REDIS_SECRET ?? '',
-}
+  REDIS_PORT: process.env.REDIS_PORT,
+  REDIS_HOST: process.env.REDIS_HOST,
+  MYSQL_ROOT_PASSWORD: process.env.MYSQL_ROOT_PASSWORD,
+  MYSQL_DATABASE: process.env.MYSQL_DATABASE,
+  MYSQL_USER: process.env.MYSQL_USER,
+  MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+  REDIS_SECRET: process.env.REDIS_SECRET ?? '',
+};
 
 export const redisClient = createClient({
-    url: `redis://${globalConfig.REDIS_HOST}:${globalConfig.REDIS_PORT}`
-})
+  url: `redis://${globalConfig.REDIS_HOST}:${globalConfig.REDIS_PORT}`,
+});
 
 export const prismaClient = new PrismaClient();
